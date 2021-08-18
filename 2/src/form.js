7b9e9697139fbb578701c029d9edc058c6484438
comav <tsyomav@gmail.com>
2021-08-18 15:40:38 +0300

    Fixed bugs and added animations

--- 2/src/form.js
+++ 2/src/form.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Loader from 'react-loader-spinner';
+import {useSpring, animated} from 'react-spring';
 import './App.css'
 
 function Form() {
+
+    const props = useSpring({ to: {opacity: 1}, from: {opacity: 0}, delay: 100})
+
     return (
-        <form>
+        <animated.form style={props}>
             <div className={'input-wrapper'}>
                 <label>Email</label>
                 <input type={'email'}></input>
@@ -19,8 +24,53 @@ function Form() {
                 <input type={'checkbox'} className={'checkbox'}></input>
                 <p>Stay signed in for a week</p>
             </div>
-            <button className={'button'}>Continue</button>
-        </form>
+            <Example></Example>
+        </animated.form>
+    );
+}
+
+function Button({isLoading, children, ...props}) {
+
+    const [showLoader, setShowLoader] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isLoading) {
+          setShowLoader(true);
+        }
+      
+        // Show loader a bits longer to avoid loading flash
+        if (!isLoading && showLoader) {
+          const timeout = setTimeout(() => {
+            setShowLoader(false);
+          }, 1500);
+      
+          // Don’t forget to clear the timeout
+          return () => {
+            clearTimeout(timeout);
+          };
+        }
+      }, [isLoading, showLoader]);
+
+    return (
+        <button className={'button'} {...props}>
+            {isLoading ? <Loader color={'#fff'} type={'ThreeDots'} height={'30'} width={'50'} /> : 'Continue'}
+        </button>
+    )
+}
+
+function Example () {
+    const [isButtonLoading, setIsButtonLoading] = React.useState(false);
+  
+    return (
+      <Button
+        onClick={() => {
+          setIsButtonLoading(true);
+          setTimeout(() => {
+            setIsButtonLoading(false);
+          }, 1000);
+        }}
+        isLoading={isButtonLoading}
+      />
     );
 }
 
